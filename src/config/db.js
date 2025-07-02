13d3d7dede86357cb6f5000e06a01666df868e54
neetunabbu <nabeelafatima411@gmail.com>
2025-07-02 16:48:56 +0530

    update commit

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
 // ✅ Create Sequelize instance
-const sequelize = new Sequelize('zestfindz', 'postgres', 'Kumar@2000', {
+const sequelize = new Sequelize('zestfindz_DB', 'postgres', '9700912007', {
     host: 'localhost',
     dialect: 'postgres',
     logging: false, // Disable SQL logging in terminal
