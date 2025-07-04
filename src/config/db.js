6a9b95493ffdea8156c91f6987accf42215b4cb7
yesukumar bhuvanagiri <yesukumarfd@techlanditsolutions.com>
2025-07-04 10:17:55 +0530

    updated env values

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,17 @@
 const { Sequelize } = require('sequelize');
 
 // ✅ Create Sequelize instance
-const sequelize = new Sequelize('zestfindz_DB', 'postgres', '9700912007', {
-    host: 'localhost',
+const sequelize = new Sequelize(
+  process.env.DB_NAME || 'zestfindz',
+  process.env.DB_USER || 'postgres',
+  process.env.DB_PASS || 'Kumar@2000',
+  {
+    host: process.env.DB_HOST || 'localhost',
     dialect: 'postgres',
     logging: false, // Disable SQL logging in terminal
     timezone: '+05:30' // Set to your local timezone if needed
-});
+  }
+);
 
 // ✅ Test Database Connection
 (async () => {
