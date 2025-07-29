398d0e3367abe6ca5748ccf163a1136fd6f9c7a0
yesukumar bhuvanagiri <yesukumarfd@techlanditsolutions.com>
2025-07-29 18:05:18 +0530

    Comment out BlogController and blog review route
    BlogController import and the blog review route have been commented out in apiRoutes.js, likely to temporarily disable blog review functionality.

--- src/routes/api/v1/Dashboard/user/apiRoutes.js
+++ src/routes/api/v1/Dashboard/user/apiRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const sequelize = require('../../../../../config/db');
-const BlogController = require('../../../../../controllers/Api/v1/dashboard/user/BlogController');
+// const BlogController = require('../../../../../controllers/Api/v1/dashboard/user/BlogController');
 const DigitalFileController = require('../../../../../controllers/Api/v1/dashboard/user/DigitalFileController');
 const verifyToken = require('../../../../../middleware/verifyToken');
 const upload = require('../../../../../middleware/upload');
@@ -23,7 +23,7 @@ router.put('/user/userprofile/:id', UserProfileController.update);
 router.delete('/user/userprofile/:id', UserProfileController.destroy);
 // Review Routes Here
 // router.post('/user/shops/review/:id', ShopController.addReview);
-router.post('/user/blogs/review/:id', BlogController.addReview);
+// router.post('/user/blogs/review/:id', BlogController.addReview);
 // Digital File Routes Here
 router.get('/user/digital-files',  DigitalFileController.index);
 router.get('/user/my-digital-files',  DigitalFileController.myDigitalFiles);
