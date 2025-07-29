1edd7899d021a418f0f22a9377da98bbb66fe7ac
yesukumar bhuvanagiri <yesukumarfd@techlanditsolutions.com>
2025-07-29 18:18:25 +0530

    Fix case sensitivity in Loggable import
    Corrected the import path for Loggable to use the proper case, addressing potential issues on case-sensitive file systems. Commented out the unused ApiResponse import.

--- src/services/CoreService.js
+++ src/services/CoreService.js
@@ -2,8 +2,8 @@
 
 const { Currency, Language } = require('../models');
 const ResponseError = require('../helpers/ResponseError');
-const ApiResponse = require('../Traits/ApiResponse');
-const Loggable = require('../Traits/loggable');
+// const ApiResponse = require('../Traits/ApiResponse');
+const Loggable = require('../Traits/Loggable');
 const cache = require('../utils/cache'); // Assume cache utility (e.g., NodeCache or Redis)
 
 class CoreService {
